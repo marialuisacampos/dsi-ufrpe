1918f1f597a5e628e866152d4f0585d3a71f1b02
marialuisacampos <luisa.campos@ufrpe.br>
2022-12-11 19:53:58 -0300

    Add icons to the list

--- dsi_ufrpe/lib/main.dart
+++ dsi_ufrpe/lib/main.dart
@@ -9,6 +9,7 @@ void main() {
   runApp(const MyApp());
 }
 
+//configurações gerais do aplicativo
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+////////////
 
 class RandomWords extends StatefulWidget {
   const RandomWords({super.key});
@@ -37,6 +39,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
@@ -46,6 +49,12 @@ class _RandomWordsState extends State<RandomWords> {
         if (i.isOdd) return const Divider();
 
         final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        final alreadySaved = _saved.contains(_suggestions[index]);
+
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
@@ -53,6 +62,11 @@ class _RandomWordsState extends State<RandomWords> {
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
           ),
         );
       },
